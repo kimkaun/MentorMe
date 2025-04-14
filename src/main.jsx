22de30cdf3b7667ae2d47c8b7153ff4c024fea47
82107 <82107@>
2025-04-14 22:06:12 +0900

    로그인 인풋, 링크, 버튼 구현

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client';
+// import App from './App.jsx';
+import LoginPage from './Login_SignUp/components/LoginPage';
+
+createRoot(document.getElementById('root')).render(<LoginPage/>);
